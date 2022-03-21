07e301ea4d9de27ae52705ba3a3342f39c0ef466
grand-force <grandforce8188@gmail.com>
2022-03-21 18:04:00 -0500

    Updates

---
+++ src/main/java/frc/robot/commands/ShooterSetPercentOutput.java
@@ -0,0 +1,31 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
+
+public class ShooterSetPercentOutput extends CommandBase {
+  private final Shooter mShooter;
+  private int mOutput;
+
+  public ShooterSetPercentOutput(Shooter shooter, int output) {
+    mShooter = shooter;
+    mOutput = output;
+    addRequirements(mShooter);
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    mShooter.setShooterSpeed(mOutput);
+  }
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
